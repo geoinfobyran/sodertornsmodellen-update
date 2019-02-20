59125b464303195a63884e92317886d76e24afa2
johnniehard <johnnie@synvinkel.org>
2019-02-20 09:17:01 +0100

    Add comments to each section of the helpers file

--- helpers.py
+++ helpers.py
@@ -2,23 +2,22 @@ import pandas as pd
 import numpy as np
 import os
 
+# Set up input and output directories
+# input
 indataPath = os.path.join(os.pardir, "indata")
-
 ddfRootPath = os.path.join(indataPath, "ddf--sodertornsmodellen")
 ddfSrcPath = os.path.join(ddfRootPath, "ddf--sodertornsmodellen--src")
-
 superPath = os.path.join(indataPath, "supermappen")
-
+# output
 ddfOutputPath = os.path.join(os.pardir, 'ddf--sodertornsmodellen-output', 'ddf--sodertornsmodellen--src')
 
 # Ta året ur filnamnen i supermappen
 def getYear(fileName):
     return fileName[-8:-4]
 
-# Ta bort fil, strunta i det om den inte finns
+# Remove file, without throwing errors if the file does not exist
 # https://stackoverflow.com/a/10840586
 import os, errno
-
 def silentremove(filename):
     try:
         os.remove(filename)
@@ -26,6 +25,7 @@ def silentremove(filename):
         if e.errno != errno.ENOENT:
             raise
 
+# Append new data to concept. Return the combined datasets for plotting.
 def appendNewDatapoints(concept, _df):
     filename = 'ddf--datapoints--{concept}--by--basomrade--year.csv'.format(concept=concept)
     previousPath = os.path.join(ddfSrcPath, filename)
@@ -40,6 +40,7 @@ def appendNewDatapoints(concept, _df):
     combined = combined.rename(columns={concept: 'value'})
     return combined
 
+# Append new data to concept by gender.
 def byGender(concept, _df):
     genders = _df['Kön'].cat.categories.tolist()
 
@@ -54,24 +55,27 @@ def byGender(concept, _df):
         combinedGenders.append(combined)
     return combinedGenders[0], combinedGenders[1]
 
+# Lookup tables between BASKOD2000 and BASKOD2010
 baskodkey = pd.read_excel(os.path.join(ddfRootPath, 'etl', 'source', '161115 A7 utan formler.xlsx'), skiprows=[0,1,2,3,4,5,6], converters={2010: lambda x: str(x)})
 baskodkey = baskodkey[[2010, 'BASKOD2000']]
 baskodkey = baskodkey.rename(columns={2010: 'BASKOD2010'})
 baskodkey['BASKOD2010'] = baskodkey['BASKOD2010'].astype(str).astype(int)
 
-
+# Lookup table between BASKOD2000 and the baskod entity id.
 entityKey = pd.read_csv(os.path.join(ddfOutputPath, 'ddf--entities--basomrade.csv'))
 entityKey = entityKey.rename(columns={'baskod2000': 'BASKOD2000'})
 entityKey['basomrade'] = entityKey['basomrade'].astype(str)
-
 entityKey = entityKey[['basomrade', 'BASKOD2000']]
 
+# Use the above lookup tables to go from BASKOD2010 to baskod entity id.
 def baskod2010tobasomrade(_df):
     _df = pd.merge(_df, baskodkey, on='BASKOD2010', how='left')
     _df = pd.merge(_df, entityKey[['BASKOD2000', 'basomrade']], on='BASKOD2000', how='left')
     _df = _df.dropna(how='any')
     return _df
 
+# Plot timeseries of the combined old+update dataframes.
+# Sums all values, rates won't mean much. The point is to get a quick visual indication of errors, such as sudden jumps between last year in old data and first year in the new data.
 def plotcombined(combined, name='value'):
     combined = combined.rename(columns={'value': name})
     combined['year'] = pd.to_datetime(combined['year'].astype('str'))
